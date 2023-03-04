0820635d60f3da65232fdcbd095916b1eff73f26
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-03-04 10:35:44 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ Romeo/plugins/restart.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import asyncio
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from Romeo import SUDO_USER
+
+@Client.on_message(
+    filters.command(["restart", "reload", "rs", "rl"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def restart(client: Client, message: Message):
+    reply = await message.reply_text("**Restarting...**")
+    await message.delete()
+    await reply.edit_text("Successfully Restarted RomeoBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    os.system(f"kill -9 {os.getpid()} && python3 -m Romeo")
