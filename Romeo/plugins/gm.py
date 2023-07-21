1e977d0f6d096cf1fa88f3bf061cf728bd2f3aff
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-07-21 18:46:48 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ Romeo/plugins/gm.py
@@ -0,0 +1,60 @@
+from asyncio import sleep
+from random import choice
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Romeo.helper.gm import *
+from Romeo import spam_chats
+
+
+@Client.on_message(filters.command(["morning", "gm", "g"], [".", "?", "/"]) & filters.me)
+async def gm(client: Client, message: Message):
+    chat_id = message.chat.id
+    reply = GM
+    await message.delete()
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
+        if usrnum == 4:
+            if reply:   
+                r = choice(GM)
+                txt = f"{r}\n\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@Client.on_message(filters.command(["smorning", "sgm", "sg"], [".", "?", "/"]) & filters.me)
+async def gm(client: Client, message: Message):
+    chat_id = message.chat.id
+    reply = GM
+    await message.delete()
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
+        if usrnum == 1:
+            if reply:   
+                r = choice(GM)
+                txt = f"{r}\n\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
