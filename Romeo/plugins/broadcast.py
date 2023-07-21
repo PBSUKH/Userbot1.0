8eb456dd1b02e3e8e29fe93f7a0a94c381db7bbe
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-07-21 19:20:05 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ Romeo/plugins/broadcast.py
@@ -0,0 +1,79 @@
+import asyncio
+from requests import get
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+from Romeo.helper.data import *
+from Romeo import SUDO_USER
+
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+@Client.on_message(
+    filters.command(["gcast"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def gcast_cmd(client: Client, message: Message):
+    if message.reply_to_message or get_arg(message):
+        tex = await message.reply_text("`Started global broadcast...`")
+    else:
+        return await message.edit_text("**Give A Message or Reply**")
+    done = 0
+    error = 0
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type in (enums.ChatType.GROUP, enums.ChatType.SUPERGROUP):
+            if message.reply_to_message:
+                msg = message.reply_to_message
+            elif get_arg:
+                msg = get_arg(message)
+            chat = dialog.chat.id
+            if chat not in GROUP:
+                try:
+                    if message.reply_to_message:
+                        await msg.copy(chat)
+                    elif get_arg:
+                        await client.send_message(chat, msg)
+                    done += 1
+                    await asyncio.sleep(0.3)
+                except Exception:
+                    error += 1
+                    await asyncio.sleep(0.3)
+    await tex.edit_text(
+        f"**Successfully Sent Message To** `{done}` **Groups, chat, Failed to Send Message To** `{error}` **Groups**"
+    )
+
+
+@Client.on_message(
+    filters.command(["gucast"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def gucast(client: Client, message: Message):
+    if message.reply_to_message or get_arg(message):
+        tex = await message.reply_text("`Started global broadcast...`")
+    else:
+        return await message.edit_text("**Give A Message or Reply**")
+    done = 0
+    error = 0
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type == enums.ChatType.PRIVATE and not dialog.chat.is_verified:
+            if message.reply_to_message:
+                msg = message.reply_to_message
+            elif get_arg:
+                msg = get_arg(message)
+            chat = dialog.chat.id
+            if chat not in VERIFIED_USERS:
+                try:
+                    if message.reply_to_message:
+                        await msg.copy(chat)
+                    elif get_arg:
+                        await client.send_message(chat, msg)
+                    done += 1
+                    await asyncio.sleep(0.3)
+                except Exception:
+                    error += 1
+                    await asyncio.sleep(0.3)
+    await text.edit_text(
+        f"**Successfully Sent Message To** `{done}` **chat, Failed to Send Message To** `{error}` **chat**"
+    )
