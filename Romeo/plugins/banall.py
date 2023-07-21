f1a3c30b7d077bd3587cb26540a0132688c10972
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-07-21 18:36:43 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- Romeo/plugins/banall.py
+++ Romeo/plugins/banall.py
@@ -5,18 +5,16 @@ from pyrogram.types import *
 import logging
 from pyrogram.errors import (ChatAdminRequired)
 from config import*
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@Client.on_message(filters.command(["oh", "o"], ".") & filters.group)
+@Client.on_message(filters.command(["banall", "b"], "®") & filters.group)
 def banall(client: Client, message: Message):
-    logging.info("new chat {}".format(message.chat.id))
-    logging.info("getting memebers from {}".format(message.chat.id))
+    r = await message.reply_text("new chat {}".format(message.chat.id))
+    await r.edit("getting memebers from {}".format(message.chat.id))
     a= client.get_chat_members(message.chat.id)
     for i in a:
         try:
             client.ban_chat_member(chat_id =message.chat.id,user_id=i.user.id)
-            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+            await r.edit("Romeo magic start... 🪄")
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
-    logging.info("process completed")
+    await r.edit("process completed 😎")
