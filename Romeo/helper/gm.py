de776704acd57d6645d15a2bfd5342e18a266d3c
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-07-21 18:41:40 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ Romeo/helper/gm.py
@@ -0,0 +1,8 @@
+GM = [
+    "𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆 𝐆𝐔𝐘𝐒 𝐇𝐀𝐕𝐄 𝐀 𝐆𝐑𝐄𝐀𝐓 𝐃𝐀𝐘 ☀️🌞☀️🌞",
+    "Good Morning Have A Sweet Day 🌞🌞🌞",
+    "kaab taak sowoge 😁😅",
+    "Utho yaara subah ho gyi 🥰🥰",
+    "tumhare babu sona jag gye kaab taak sowoge 😜😜",
+    "Suraj v nikal gya tum kaab nikloge 😁😁",
+]
