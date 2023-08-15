c2629c1213a7b4d99aa44301d95b1ba9829baf4f
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-08-16 01:04:39 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- Romeo/plugins/gm.py
+++ Romeo/plugins/gm.py
@@ -1,10 +1,68 @@
-from asyncio import sleep
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Romeo.helper.gm import *
+from cache.gm import *
+
+from Romeo.modules.help import add_command_help
 from Romeo import spam_chats
 
+
+@Client.on_message(filters.command(["morning", "gm", "g"], [".", "?", "/"]) & filters.me)
+async def gm(client: Client, message: Message):
+    chat_id = message.chat.id
+    reply = GGM
+    await message.delete()
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
+        if usrnum == 4:
+            if reply:   
+                S = choice(GGM)
+                p = f"ㅤ"
+                txt = f"{p}\n{usrtxt}\n{p}"
+                await client.send_video(chat_id, video=S, caption=txt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@Client.on_message(filters.command(["smorning", "sgm", "sg"], [".", "?", "/"]) & filters.me)
+async def sgm(client: Client, message: Message):
+    chat_id = message.chat.id
+    sreply = GGM
+    await message.delete()
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id})"
+        if usrnum == 1:
+            if sreply:
+                R = choice(GGM)
+                q = f"ㅤ"
+                txt = f"{q}\n{usrtxt}\n{q}"
+                await client.send_video(chat_id, video=R, caption=txt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+        
+'''
 
 @Client.on_message(filters.command(["morning", "gm", "g"], [".", "?", "/"]) & filters.me)
 async def gm(client: Client, message: Message):
@@ -58,32 +116,5 @@ async def gm(client: Client, message: Message):
         spam_chats.remove(chat_id)
     except:
         pass
-
-"""
-@Client.on_message(filters.command(["smorning", "sgm", "sg"], [".", "?", "/"]) & filters.me)
-async def sgm(client: Client, message: Message):
-    chat_id = message.chat.id
-    sreply = GGM
-    await message.delete()
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.get_chat_members(chat_id):
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id})"
-        if usrnum == 1:
-            if sreply:
-                R = choice(GGM)
-                txt = f"{usrtxt}"
-                await client.send_video(chat_id, video=R, caption=txt)
-            await sleep(2)
-            usrnum = 0
-            usrtxt = ""
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
-"""        
+        
+'''
