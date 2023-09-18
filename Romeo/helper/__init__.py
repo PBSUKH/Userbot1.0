1547fbb9a766c54f698d7a2b8e5c7d53710d9c78
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-09-18 18:30:38 +0530

    😎

--- Romeo/helper/__init__.py
+++ Romeo/helper/__init__.py
@@ -1,10 +1,1 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo import MongoClient 
-from pyrogram import Client 
-from config import MONGO_URL
 
-r = AsyncIOMotorClient(MONGO_URL) 
-j = MongoClient(MONGO_URL) 
-mongodb = r.Romeo
-pymongodb = r.Romeo
-dbb = r["ROMEODB"]
