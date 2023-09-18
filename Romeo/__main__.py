ffc75a9c3f1d5607902ed846e764b8da73f9c4e3
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-09-18 18:08:35 +0530

    😎

--- Romeo/__main__.py
+++ Romeo/__main__.py
@@ -2,23 +2,13 @@ import asyncio
 import importlib
 from pyrogram import Client, idle
 from Romeo.plugins import ALL_MODULES
-from Romeo import clients, app, ids
+from Romeo import client, app
 
 async def start_bot():
     await app.start()
     print("LOG: Founded Bot token Booting..")
-    for all_module in ALL_MODULES:
-        importlib.import_module("Romeo.plugins" + all_module)
-        print(f"Successfully Imported {all_module} 🧐")
-    for cli in clients:
-        try:
-            await cli.start()
-            ex = await cli.get_me()
-            await cli.join_chat("RomeoBot_op")
-            print(f"Started {ex.first_name} 🔥")
-            ids.append(ex.id)
-        except Exception as e:
-            print(f"{e}")
+    print("USERBOT SUCCESSFULLY STARTED ✅✅")
+    await client.start()
     await idle()
 
 loop = asyncio.get_event_loop()
