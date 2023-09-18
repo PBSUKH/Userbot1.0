489ca2b5f43e4b36bf366c9fab88bf47a1202934
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-09-18 18:28:42 +0530

    😎

--- Romeo/__init__.py
+++ Romeo/__init__.py
@@ -1,13 +1,9 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION
-from datetime import datetime
-import time
-from aiohttp import ClientSession
 
 spam_chats = []
 SUDO_USER = SUDO_USERS
 SUDO_USERS.append(OWNER_ID)
-aiosession = ClientSession()
 
 app = Client(
     name="app",
@@ -18,15 +14,12 @@ app = Client(
     in_memory=True,
 )
 
-if not API_ID:
-   print("WARNING ⚠️  API_ID NOT FOUND PLZ ADD.")
+client = Client(
+    name="rj",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    session_string=STRING_SESSION,
+    plugins=dict(root="Romeo/plugins")
+)
 
-if not BOT_TOKEN:
-   print("WARNING ⚠️  API_HASH NOT FOUND PLZ ADD.")
-
-if not BOT_TOKEN:
-   print("WARNING ⚠️ BOT TOKEN NOT FOUND PLZ ADD.")
-   
-if STRING_SESSION:
-   print("[INFO] STRING_SESSION: Found.. Starting.. Client..")
-   client = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, plugins=dict(root="Romeo/plugins"))
