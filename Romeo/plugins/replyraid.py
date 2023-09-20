841e17f0a09a5b5012ac8ff005772114588e44f6
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-09-21 01:19:30 +0530

    Update replyraid.py

--- Romeo/plugins/replyraid.py
+++ Romeo/plugins/replyraid.py
@@ -1,99 +1,84 @@
-import asyncio
-import random
-import asyncio
-import time
-from typing import Tuple
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+from random import choice
 from pyrogram import filters, Client
-from traceback import format_exc
-from typing import Tuple
-
-from Romeo.helper.data import *
+from pyrogram.types import Message
+from Romeo.helper.data import RAID, VERIFIED_USERS, GROUP
 from Romeo import SUDO_USER
 
-SUDO_USERS = SUDO_USER
+ACTIVATE_RLIST = []
 
-#LOCAL STORAGE
-ACTIVATE_LIST = []
-
-
-# CREATE LINK
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-
-#REPLYLOVERAID
-@Client.on_message(
-    filters.command(["rr", "replyraid", "raidreply"], ".") & (filters.me | filters.user(SUDO_USER))
-)
-async def rlr(client: Client, message: Message):
-    r= await message.edit_text("**Processing**")
+@Client.on_message(filters.command(["rr", "replyraid", "raidreply"], ".") & (filters.me | filters.user(SUDO_USER)))
+async def rr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
     reply = message.reply_to_message
     if reply:
         user = reply.from_user.id
     else:
-        user = get_arg(message)
+        user = message.text.split(None, 1)[1]
         if not user:
             await r.edit("**Provide Me A USER_ID or reply to someone**")
             return
     user = await client.get_users(user)
     if int(message.chat.id) in GROUP:
-        await r.edit("`You Cannnot Spam In DeadlyChats`")
+        await r.edit("`You Cannot Spam In Developers' Chat`")
         return
     if int(user.id) in VERIFIED_USERS:
         await r.edit("You Cannot Spam On Developers")
         return
-    elif int(user.id) in SUDO_USERS:
+    elif int(user.id) in SUDO_USER:
         await r.edit("That Guy Is part of sudo user.")
         return
-    elif int(user.id) in ACTIVATE_LIST:
+    elif int(user.id) in ACTIVATE_RLIST:
         await r.edit("User Already in Raidlist.")
         return
-    ACTIVATE_LIST.append(user.id)
+    ACTIVATE_RLIST.append(user.id)
     await r.edit(f"**Replyraid Activated On {user.first_name} Successfully ✅**")
 
-
-#DREPLYLOVERAID
 @Client.on_message(filters.command(["drr", "dreplyraid", "draidreply"], ".") & (filters.me | filters.user(SUDO_USER)))
-async def drlr(client: Client, message: Message):
-    r= await message.edit_text("**Processing**")
+async def drr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
     reply = message.reply_to_message
     if reply:
         user = reply.from_user.id
     else:
-        user = get_arg(message)
+        user = message.text.split(None, 1)[1]
         if not user:
-            await r.edit("Provide me username/userid or reply to user for deactivating lovereplyraid")
+            await r.edit("Provide me username/userid or reply to user for deactivating replyraid")
             return
     user = await client.get_users(user)
-    if int(user.id) not in ACTIVATE_LIST:
+    if int(user.id) not in ACTIVATE_RLIST:
         await r.edit("User Not in Replyraid.")
         return
-    ACTIVATE_LIST.remove(user.id)
+    ACTIVATE_RLIST.remove(user.id)
     await r.edit(f"**Reply Raid has Been Removed {user.first_name}, enjoy!**")
 
 
-#WATCHER
-@Client.on_message(filters.all)
-async def check_del(client: Client, message: Message):
-    if not message:
-        return
-    if int(message.chat.id) in GROUP:
-        return
+@Client.on_message(filters.incoming)
+async def watch_raids(client: Client, message: Message):
     try:
-        if not message.from_user.id in ACTIVATE_LIST:
+        if not message:
             return
-    except AttributeError:
-        return
-    try:
-        await message.reply_text(f"{random.choice(RAID)}")
-    except:
-        pass
-        return
+        if not message.from_user:
+            return
+        user = message.from_user.id
+        userr = message.from_user
+        mention = f"[{userr.first_name}](tg://user?id={userr.id})"
+        raid = f"{mention} {choice(RAID)}"
+        if int(user) in VERIFIED_USERS:
+            return
+        elif int(user) in SUDO_USER:
+            return
+        if int(message.chat.id) in GROUP:
+            return
+
+        try:
+            if not message.from_user.id in ACTIVATE_RLIST:
+                return
+        except AttributeError:
+            return
+        try:
+            if message.from_user.id in ACTIVATE_RLIST:
+                await message.reply_text(raid)
+        except Exception as a:
+            print(f"An error occurred (a): {str(a)}")
+    except Exception as b:
+        print(f"An error occurred (b): {str(b)}")
