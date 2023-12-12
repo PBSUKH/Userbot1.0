3345bb520d24786da2d85f706be30aff3993eee6
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-12 19:41:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "13335263"))
+API_ID = int(getenv("API_ID", "29908998"))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", ""))
 STRING_SESSION = getenv("STRING_SESSION", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5392070730").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
-REPO_URL = getenv("REPO_URL", "https://github.com/RRomeo-RJ/Romeo-UserBot")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/e25a563619c653328830d.jpg")
+REPO_URL = getenv("REPO_URL", "https://github.com/badmunda98/BadXuserbot")
 BRANCH = getenv("BRANCH", "main")
